b5bd8934b07c19edee4acf6f4e7fe30d11a697bc
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-12-01 16:02:58 +0800

    ArrayController
    ArrayController

---
+++ MACOS_ArrayController/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  MACOS_ArrayController
+//
+//  Created by sycf_ios on 2016/12/1.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class Person: NSObject {
+    var name = "小六"
+    var age = 24
+    
+}
